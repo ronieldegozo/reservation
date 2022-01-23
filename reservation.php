9f14e08b1748fad82f812c82c0365e5eb224eadb
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-01-23 20:22:27 +0800

    Displaying Product details on reservation pages

--- reservation.php
+++ reservation.php
@@ -33,8 +33,8 @@
 							</a>
 						</div>
 					</div> -->
-					<div class="col-md-8 ">
-						<div class="booking-form "  >
+					<div class="col mt-5 ">
+						<div class="booking-form">
 							<form action="reservation_save.php" method="POST">
 								<label for="Title"><h1 style="text-align: center;">Reserve Table</h1></label>
 								<div class="row">
@@ -72,14 +72,15 @@
 											<input type="email" class="form-control" placeholder="Email Address" name="email">
 										</div>
 									</div>
+								</div>
+								<h1>Event Details</h1>
+								<div class="row">
 									<div class="col-md-6">
 										<div class="form-group">
 											<span class="form-label">Name of Event</span>
 											<input type="text" class="form-control" placeholder="Event Name" name="r_eventname">
 										</div>
 									</div>
-								</div>
-								<div class="row">
 									<div class="col-md-6">
 										<div class="form-group">
 											<span class="form-label">Date of event</span>
@@ -90,12 +91,77 @@
 										<div class="form-group">
 											<span class="form-label">Time of Event</span>
 											<input data-format="hh:mm A" class="form-control" type="time" name="time" id="datepicker" required="true">
+					 					</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-6">
+
+									<div class="form-group">
+    									<label class="col-lg-2 control-label"></label>
+        									<div class="col-lg-5">
+										<?php
+											include('includes/dbcon.php');
+    										$query=mysqli_query($con,"select * from combo order by combo_name")or die(mysqli_error($con));
+      										$count=mysqli_num_rows($query);
+    										while ($row=mysqli_fetch_array($query)){
+        									$id=$row['combo_id'];
+        									$name=$row['combo_name'];
+        									$price=$row['combo_price'];  
+										?>     
+         							<div class="col-md-6">
+              							<div class="widget">
+                							<!-- Widget title -->
+											<!-- PRICE -->
+               								<div class="widget-head">
+												<div class="pull-left"><?php echo $name;?> - P<?php echo $price;?></div>
+													<div class="widget-icons pull-right"></div>  
+												<div class="clearfix"></div>
+                							</div>
+                					<div class="widget-content referrer">
+                  							<!-- Widget content -->
+                 						<table class="table table-striped table-bordered table-hover">
+                    				<tbody>
+										<?php
+											$query1=mysqli_query($con,"select * from combo_details natural join menu where combo_id='$id'")or die(mysqli_error($con));
+											while ($row1=mysqli_fetch_array($query1)){
+											$cid=$row1['combo_details_id'];
+											$menu_id=$row1['menu_id'];
+											$menu_name=$row1['menu_name'];
+										?>                        
+											<tr>
+												<td><?php echo $menu_name;?></td>
+											</tr>     
+										<?php }?>                    
+                 		 			</tbody>
+										</table>
+										<div class="widget-foot text-center">
+											<input type="radio" id="inlineCheckbox1" value="<?php echo $id;?>" name="combo_id">
 										</div>
+                					</div>
+             	 						</div>
+            						</div>
+              						<!--end widget-->
+            					<?php }?>  
+         </div>
+      </div> 
 									</div>
 								</div>
 
 								<!-- data -->
-				
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-btn return">
+											<a href="./index.php">Return</a>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-btn">
+											<button class="submit-btn btn btn-success" type="submit">Add Reservation</button>
+										</div>
+									</div>
+								</div>
+								
 								</div>
 								<!-- <div class="row">
 									<div class="col-md-4">
@@ -132,9 +198,6 @@
 										</div>
 									</div>
 								</div> -->
-								<div class="form-btn">
-									<button class="submit-btn" type="submit">Add Reservation</button>
-								</div>
 							</form>
 						</div>
 					</div>
